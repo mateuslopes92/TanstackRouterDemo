9c9240ef56c12f036903b4187ada40906423888b
mateuslopes92 <mateuslopes92@gmail.com>
2026-03-31 01:01:49 -0300

    TanstackRouter: Added routes folder and routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,24 @@
 import './App.css'
 
+import { RouterProvider, createRouter } from '@tanstack/react-router'
+
+import { routeTree } from './routeTree.gen'
+
+// routerTree is a auto generate file that contains all the routes in the src/routes directory
+const router = createRouter({ routeTree })
+
+// expand the interface to make autocomplete work with the router
+declare module '@tanstack/react-router' {
+  export interface RegisterRouter {
+    router: typeof router
+  }
+}
+
 function App() {
 
   return (
-    <>
-      Tanstack works!
-    </>
+    <RouterProvider router={router} >
+    </RouterProvider>
   )
 }
 
