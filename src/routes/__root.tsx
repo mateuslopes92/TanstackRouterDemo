498bfeb6b3fe30b18b78192780aa0dc28898d8ca
mateuslopes92 <mateuslopes92@gmail.com>
2026-04-03 01:18:17 -0300

    Added Navigation to explore Link component

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -1,5 +1,19 @@
-import { Outlet, createRootRoute } from "@tanstack/react-router";
+import { Link, createRootRoute } from "@tanstack/react-router";
+
+const activeProps = { style: { fontWeight: 'bold' } }
 
 export const Route = createRootRoute({
-  component: () => <Outlet />
-})
+  component: () => (
+    <>
+      <h1>My Tanstack App</h1>
+      <ul>
+        <li>
+          <Link to="/" activeProps={activeProps}> Home</Link>
+        </li>
+        <li>
+          <Link to="/profile" activeProps={activeProps}>{({ isActive }) => <>Profile {isActive && ' (Active)'}</>}</Link>
+        </li>
+      </ul>
+    </>
+  )
+});
