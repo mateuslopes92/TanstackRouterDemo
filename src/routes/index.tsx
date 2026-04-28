a159cf10573f51d7833ba049f1ca0bc665d90d1b
mateuslopes92 <mateuslopes92@gmail.com>
2026-04-27 23:38:53 -0300

    TanstackRouter: Change index file text

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -3,11 +3,11 @@ import { createFileRoute } from "@tanstack/react-router";
 export const Route = createFileRoute("/")({
   component: () => (
     <div className="page">
-      <h1 className="page__title">Welcome Home</h1>
+      <h1 className="page__title">Welcome To Tanstack Router Demo</h1>
       <div className="page__card">
         <p className="page__text">
-          This is the home page of your TanStack Router demo application.
-          Use the navigation above to explore the app.
+          This is the home page of your TanStack Router demo application. <br />
+          Use the navigation above to explore the app, and find out more about each Pokemon!
         </p>
       </div>
     </div>
