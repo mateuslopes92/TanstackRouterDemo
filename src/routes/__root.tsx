e89a7dc0a892a0ed4304b14bb9ae8edc121bb84d
mateuslopes92 <mateuslopes92@gmail.com>
2026-06-21 23:25:22 -0300

    TanstackRouter: added code for css for pokemon

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -14,67 +14,76 @@ export const Route = createRootRouteWithContext<RouterContext>()({
   component: () => (
     <>
       <header className="header">
-        <h1 className="header__title">Pokemon Tanstack Router App</h1>
-        <nav>
-          <ul className="header__nav">
-            <li>
-              <Link to="/" className="header__link" activeProps={activeProps}>
-                Home
-              </Link>
-            </li>
-            <li>
-              <Link
-                to="/pokemon"
-                className="header__link"
-                activeProps={activeProps}
-              >
-                {({ isActive }) => (
-                  <span className={isActive ? "header__link--active" : ""}>
-                    Pokemons {isActive && "(You are here)"}
-                  </span>
-                )}
-              </Link>
-            </li>
-            <li>
-              <Link
-                to="/search"
-                className="header__link"
-                activeProps={activeProps}
-              >
-                Search
-              </Link>
-            </li>
+        <div className="header__inner">
+          <div className="header__left">
+            <img
+              src="/pokeball.svg"
+              alt="Pokeball"
+              className="header__pokeball"
+            />
+            <h1 className="header__title">Pokemon Tanstack Router App</h1>
+          </div>
+          <nav>
+            <ul className="header__nav">
+              <li>
+                <Link to="/" className="header__link" activeProps={activeProps}>
+                  Home
+                </Link>
+              </li>
+              <li>
+                <Link
+                  to="/pokemon"
+                  className="header__link"
+                  activeProps={activeProps}
+                >
+                  {({ isActive }) => (
+                    <span className={isActive ? "header__link--active" : ""}>
+                      Pokemons {isActive && "(You are here)"}
+                    </span>
+                  )}
+                </Link>
+              </li>
+              <li>
+                <Link
+                  to="/search"
+                  className="header__link"
+                  activeProps={activeProps}
+                >
+                  Search
+                </Link>
+              </li>
 
-            <li className="header__nav--spacer" />
-            <li>
-              <Link
-                to="/auth"
-                className="header__link"
-                activeProps={activeProps}
-              >
-                Auth
-              </Link>
-            </li>
-            <li>
-              <Link
-                to="/profile"
-                className="header__link"
-                activeProps={activeProps}
-              >
-                Profile
-              </Link>
-            </li>
-            <li>
-              <Link
-                to="/settings"
-                className="header__link"
-                activeProps={activeProps}
-              >
-                Settings
-              </Link>
-            </li>
-          </ul>
-        </nav>
+              <li className="header__nav--spacer" />
+              <li>
+                <Link
+                  to="/auth"
+                  className="header__link"
+                  activeProps={activeProps}
+                >
+                  Auth
+                </Link>
+              </li>
+              <li>
+                <Link
+                  to="/profile"
+                  className="header__link"
+                  activeProps={activeProps}
+                >
+                  Profile
+                </Link>
+              </li>
+              <li>
+                <Link
+                  to="/settings"
+                  className="header__link"
+                  activeProps={activeProps}
+                >
+                  Settings
+                </Link>
+              </li>
+            </ul>
+          </nav>
+        </div>
       </header>
       <main className="main">
         <Outlet />
