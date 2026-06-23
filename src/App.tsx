b3c2014618217dce652a2e54fcbeb51be5bef4d1
mateuslopes92 <mateuslopes92@gmail.com>
2026-06-22 23:12:05 -0300

    TanstackRouter: Added default not found

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,20 @@ import { useAuth } from './hooks/useAuth'
 // routerTree is a auto generate file that contains all the routes in the src/routes directory
 const router = createRouter({
   routeTree,
-  context: { authentication: undefined! } // The documentation says to use undefined! here
+  context: { authentication: undefined! }, // The documentation says to use undefined! here
+  defaultNotFoundComponent: () => (
+    <div className="not-found">
+      <div className="not-found__pokeball">
+        <div className="not-found__pokeball-top" />
+        <div className="not-found__pokeball-bottom" />
+        <div className="not-found__pokeball-button" />
+      </div>
+      <h1 className="not-found__code">404</h1>
+      <p className="not-found__text">Wild page not found!</p>
+      <p className="not-found__hint">The page you're looking for has fled.</p>
+      <a href="/" className="not-found__link">Go back home</a>
+    </div>
+  ),
 })
 
 // expand the interface to make autocomplete work with the router
